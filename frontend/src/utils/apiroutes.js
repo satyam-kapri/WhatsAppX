1c34201600b1f6b0a2dc2fb20c9331c7eefd3c6c
satyam-kapri <118076948+satyam-kapri@users.noreply.github.com>
2023-11-29 00:34:44 +0530

    Update server address

--- frontend/src/utils/apiroutes.js
+++ frontend/src/utils/apiroutes.js
@@ -1,4 +1,4 @@
-export const host = "http://192.168.141.45:5000";
+export const host = "https://whatsappx-server.onrender.com";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
